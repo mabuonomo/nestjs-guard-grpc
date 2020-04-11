31b1d0c78be9880e4f1b7c57f0bb4fe0cc769842
Mario Buonomo <mario.buonomo@facile.it>
2020-04-11 17:14:16 +0200

    refactoring

--- libs/nestjs-guard-grpc/src/index.ts
+++ libs/nestjs-guard-grpc/src/index.ts
@@ -1,2 +1,6 @@
 export * from './nestjs-guard-grpc.module';
 export * from './nestjs-guard-grpc.service';
+export * from './jwt/jwt.service';
+export * from './interfaces/auth.interface';
+export * from './interfaces/user.interface';
+export * from './guard/grpc.guard';
--- libs/nestjs-guard-grpc/src/nestjs-guard-grpc.module.ts
+++ libs/nestjs-guard-grpc/src/nestjs-guard-grpc.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { NestjsGuardGrpcService } from './nestjs-guard-grpc.service';
 import { JwtService } from './jwt/jwt.service';
+import { GrpcAuthGuard } from './guard/grpc.guard';
 
 @Module({
   providers: [NestjsGuardGrpcService, JwtService],
-  exports: [NestjsGuardGrpcService],
+  exports: [NestjsGuardGrpcService, GrpcAuthGuard],
 })
 export class NestjsGuardGrpcModule {}
