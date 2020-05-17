682b44e437757ecf84d4429ecbad950aca4f56e2
Mario Buonomo <mario.buonomo@facile.it>
2020-05-17 20:34:53 +0200

    publish

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './libs/nestjs-guard-grpc/src/nestjs-guard-grpc.module';
-export * from './libs/nestjs-guard-grpc/src/jwt/jwt.service';
-export * from './libs/nestjs-guard-grpc/src/interfaces/auth.interface';
-export * from './libs/nestjs-guard-grpc/src/interfaces/user.interface';
-export * from './libs/nestjs-guard-grpc/src/guard/grpc.guard';
-export * from './libs/nestjs-guard-grpc/src/decorator/user.grpc.decorator';
--- dist/libs/nestjs-guard-grpc/src/decorator/user.grpc.decorator.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const GRPCUser: (...dataOrPipes: unknown[]) => ParameterDecorator;
