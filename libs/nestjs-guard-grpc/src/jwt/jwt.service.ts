e8ab239bf51766ef78f55e861489062ad8a03984
Mario Buonomo <mario.buonomo@facile.it>
2020-05-17 20:32:49 +0200

    publish

--- libs/nestjs-guard-grpc/src/jwt/jwt.service.ts
+++ libs/nestjs-guard-grpc/src/jwt/jwt.service.ts
@@ -1,19 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
-import * as jwks from 'jwks-rsa';
 import { AuthServiceInterface } from '../interfaces/auth.interface';
+import { JwksClient, ClientOptions, RsaSigningKey } from 'jwks-rsa';
 
 export class JwtService implements AuthServiceInterface {
-  client: jwks.JwksClient;
-  constructor(options: jwks.ClientOptions) {
-    this.client = new jwks.JwksClient(options);
+  client: JwksClient;
+  constructor(options: ClientOptions) {
+    this.client = new JwksClient(options);
   }
   verify(params: any): any {
     return jwt.verify(params.token, this.getKey, params.options);
   }
 
   getKey(header: jwt.JwtHeader, callback: jwt.SigningKeyCallback) {
-    this.client.getSigningKey(header.kid, (err: Error, key: jwks.RsaSigningKey) => {
+    this.client.getSigningKey(header.kid, (err: Error, key: RsaSigningKey) => {
       const signingKey = key.getPublicKey() || key.rsaPublicKey;
       callback(null, signingKey);
     });
