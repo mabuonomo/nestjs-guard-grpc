d4c024fa2ec98dc557ec6e96a08c767bb47283ed
Mario Buonomo <mario.buonomo@facile.it>
2020-05-17 20:23:50 +0200

    publish

--- libs/nestjs-guard-grpc/src/jwt/jwt.service.ts
+++ libs/nestjs-guard-grpc/src/jwt/jwt.service.ts
@@ -3,7 +3,6 @@ import * as jwt from 'jsonwebtoken';
 import * as jwks from 'jwks-rsa';
 import { AuthServiceInterface } from '../interfaces/auth.interface';
 
-@Injectable()
 export class JwtService implements AuthServiceInterface {
   client: jwks.JwksClient;
   constructor(options: jwks.ClientOptions) {
--- libs/nestjs-guard-grpc/src/nestjs-guard-grpc.module.ts
+++ libs/nestjs-guard-grpc/src/nestjs-guard-grpc.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtService } from './jwt/jwt.service';
 
 @Module({
-  providers: [JwtService],
-  exports: [JwtService],
+  providers: [],
+  exports: [],
 })
 export class NestjsGuardGrpcModule {}
