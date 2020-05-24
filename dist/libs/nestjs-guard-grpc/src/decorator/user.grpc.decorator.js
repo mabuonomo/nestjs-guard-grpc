48206a0f93868eb36051c645bfe78f5ce37c63d5
Mario Buonomo <mario.buonomo@facile.it>
2020-05-24 16:07:16 +0200

    publish

--- dist/libs/nestjs-guard-grpc/src/decorator/user.grpc.decorator.js
+++ dist/libs/nestjs-guard-grpc/src/decorator/user.grpc.decorator.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.GRPCUser = void 0;
 const common_1 = require("@nestjs/common");
 exports.GRPCUser = common_1.createParamDecorator((data, context) => {
-    return context.switchToRpc().getContext().req.user;
+    return context.switchToRpc().getContext().user;
 });
 //# sourceMappingURL=user.grpc.decorator.js.map
--- libs/nestjs-guard-grpc/src/decorator/user.grpc.decorator.ts
+++ libs/nestjs-guard-grpc/src/decorator/user.grpc.decorator.ts
@@ -1,11 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const GRPCUser = createParamDecorator((data: unknown, context: ExecutionContext) => {
-  return context.switchToRpc().getContext().req.user;
+  return context.switchToRpc().getContext().user;
 });
-
-// @Query(returns => User)
-// @UseGuards(GqlAuthGuard)
-// whoAmI(@GRPCUser() user: User) {
-//   return this.userService.findById(user.id);
-// }
